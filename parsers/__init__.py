4a136b45c2e281671400a979d1e2d7546087fbf4
JiajinZhong77 <202430363012@mail.scut.edu.cn>
2026-07-03 23:56:02 +0800

    feat: import docs, parsers, pipeline, retrieval from data-pipeline branch

---
+++ parsers/__init__.py
@@ -0,0 +1,2 @@
+from parsers.base import BaseParser
+from parsers.registry import get_parser, parse_file, supported_extensions
---
+++ pipeline/__init__.py
@@ -0,0 +1,3 @@
+from pipeline.chunker import chunk_text
+from pipeline.embedder import embed_texts
+# process_folder 通过 'python -m pipeline.process' 或 'from pipeline.process import process_folder' 使用
